598f2c817ba1736424eabb705b60d400c8a2d77b
Bryce <Bryce.obien17@gmail.com>
2026-03-20 15:58:15 +0800

    another one

--- firebase.js
+++ firebase.js
@@ -1,10 +1,14 @@
 const admin = require("firebase-admin");
 
+const privateKey = process.env.FB_PRIVATE_KEY
+  .replace(/\\n/g, '\n')   // handles \n version
+  .replace(/\r/g, '');     // removes bad formatting
+
 const serviceAccount = {
   type: process.env.FB_TYPE,
   project_id: process.env.FB_PROJECT_ID,
   private_key_id: process.env.FB_PRIVATE_KEY_ID,
-private_key: process.env.FB_PRIVATE_KEY,
+  private_key: privateKey,
   client_email: process.env.FB_CLIENT_EMAIL,
   client_id: process.env.FB_CLIENT_ID,
 };
@@ -16,4 +20,3 @@ admin.initializeApp({
 const db = admin.firestore();
 
 module.exports = { admin, db };
-console.log("PRIVATE KEY EXISTS:", !!process.env.FB_PRIVATE_KEY); 
