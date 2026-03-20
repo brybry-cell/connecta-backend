128e9cd23debf5c69fe70d02b5c80b44d50dcb62
Bryce <Bryce.obien17@gmail.com>
2026-03-20 16:09:50 +0800

    PISTE LAST NANI PLEASE

--- firebase.js
+++ firebase.js
@@ -1,17 +1,8 @@
 const admin = require("firebase-admin");
 
-const privateKey = process.env.FB_PRIVATE_KEY
-  .replace(/\\n/g, '\n')   // handles \n version
-  .replace(/\r/g, '');     // removes bad formatting
-
-const serviceAccount = {
-  type: process.env.FB_TYPE,
-  project_id: process.env.FB_PROJECT_ID,
-  private_key_id: process.env.FB_PRIVATE_KEY_ID,
-  private_key: privateKey,
-  client_email: process.env.FB_CLIENT_EMAIL,
-  client_id: process.env.FB_CLIENT_ID,
-};
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_KEY_BASE64, "base64").toString("utf-8")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
