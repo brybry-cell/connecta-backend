f7d7165718c378178c2f47309616bfac9ae19c3d
Bryce <Bryce.obien17@gmail.com>
2026-03-26 03:54:00 +0800

    transporter

--- emailService.js
+++ emailService.js
@@ -1,10 +1,12 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // IMPORTANT
   auth: {
-    user: process.env.EMAIL_USER,      // your email
-    pass: process.env.EMAIL_PASS       // app password (NOT normal password)
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   }
 });
 
